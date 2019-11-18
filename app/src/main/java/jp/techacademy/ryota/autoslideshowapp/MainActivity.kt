b3561bba47464a5e177c860b909946ed7b3289ff
Ryota Sakurai <nkzsdy@gmail.com>
2019-11-18 13:40:49 +0900

    2秒おきに画像を切り替えるスライドショー機能を実装

--- app/src/main/java/jp/techacademy/ryota/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/ryota/autoslideshowapp/MainActivity.kt
@@ -6,11 +6,13 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +20,9 @@ class MainActivity : AppCompatActivity() {
 
     private var imageUriList = mutableListOf<Uri>()
     private var imageIndex = 0
+
+    private var mTimer: Timer? = null
+    private var mHandler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,29 +42,15 @@ class MainActivity : AppCompatActivity() {
         }
 
         prev_button.setOnClickListener {
-            if (imageUriList.isNotEmpty()) {
-                if (imageIndex == 0) {
-                    imageIndex = imageUriList.lastIndex
-                } else {
-                    imageIndex--
-                }
-                Log.d("ANDROID", "imageIndex : $imageIndex")
-                Log.d("ANDROID", "imageUri : ${imageUriList[imageIndex]}")
-                imageView.setImageURI(imageUriList[imageIndex])
-            }
+            showPrevImage()
+        }
+
+        play_pause_button.setOnClickListener {
+            togglePlayPause()
         }
 
         next_button.setOnClickListener {
-            if (imageUriList.isNotEmpty()) {
-                if (imageIndex == imageUriList.lastIndex) {
-                    imageIndex = 0
-                } else {
-                    imageIndex++
-                }
-                Log.d("ANDROID", "imageIndex : $imageIndex")
-                Log.d("ANDROID", "imageUri : ${imageUriList[imageIndex]}")
-                imageView.setImageURI(imageUriList[imageIndex])
-            }
+            showNextImage()
         }
 
         permission_request_button.setOnClickListener {
@@ -114,5 +105,55 @@ class MainActivity : AppCompatActivity() {
             imageView.setImageURI(imageUriList[imageIndex])
         }
         cursor.close()
+    }
+
+    private fun showPrevImage() {
+        if (imageUriList.isNotEmpty()) {
+            if (imageIndex == 0) {
+                imageIndex = imageUriList.lastIndex
+            } else {
+                imageIndex--
+            }
+            Log.d("ANDROID", "imageIndex : $imageIndex")
+            Log.d("ANDROID", "imageUri : ${imageUriList[imageIndex]}")
+            imageView.setImageURI(imageUriList[imageIndex])
+        }
+    }
+
+    private fun showNextImage() {
+        if (imageUriList.isNotEmpty()) {
+            if (imageIndex == imageUriList.lastIndex) {
+                imageIndex = 0
+            } else {
+                imageIndex++
+            }
+            Log.d("ANDROID", "imageIndex : $imageIndex")
+            Log.d("ANDROID", "imageUri : ${imageUriList[imageIndex]}")
+            imageView.setImageURI(imageUriList[imageIndex])
+        }
+    }
+
+    private fun togglePlayPause() {
+        if (mTimer == null) {
+            mTimer = Timer()
+
+            mTimer!!.schedule(object : TimerTask() {
+                override fun run() {
+                    mHandler.post {
+                        showNextImage()
+                    }
+                }
+            }, 2000, 2000)
+
+            prev_button.visibility = View.INVISIBLE
+            next_button.visibility = View.INVISIBLE
+            play_pause_button.text = "停止"
+        } else {
+            mTimer!!.cancel()
+            mTimer = null
+            prev_button.visibility = View.VISIBLE
+            next_button.visibility = View.VISIBLE
+            play_pause_button.text = "再生"
+        }
     }
 }
